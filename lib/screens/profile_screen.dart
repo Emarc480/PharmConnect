3aa96e9d3552fc47d86cae3a13fdf79c78125413
Mukisa Percy <mukisapercy4@gmail.com>
2026-07-26 21:18:22 +0300

    update version number

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -148,7 +148,7 @@ class ProfileScreen extends StatelessWidget {
             const SizedBox(height: 16),
             Center(
               child: Text(
-                'PharmConnect · v1.2.3',
+                'PharmConnect · v1.3.0',
                 style: TextStyle(color: Colors.grey.shade400, fontSize: 12),
               ),
             ),
